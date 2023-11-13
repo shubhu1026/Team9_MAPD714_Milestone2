ac85659104c84b2829039f28c780b1b0aad05ddc
Shubham Patel <shubhampatel@shubhams-mbp.home>
2023-11-13 16:33:54 -0500

    New Storyboards

---
+++ Team9_MAPD714_Project-Milestone2/PassengerDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PassengerDetailsViewController.swift
+//  Team9_MAPD714_Project-Milestone2
+//
+//  Created by Shubham Patel on 2023-11-13.
+//
+
+import UIKit
+
+class PassengerDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
