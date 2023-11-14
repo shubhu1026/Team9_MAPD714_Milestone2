fc3cfcc4d416dcd9d0d8588421bab9f91d059752
Shubham Patel <shubhampatel@shubhams-mbp.home>
2023-11-13 23:54:06 -0500

    payment page

--- Team9_MAPD714_Project-Milestone2/CheckoutViewController.swift
+++ Team9_MAPD714_Project-Milestone2/CheckoutViewController.swift
@@ -7,7 +7,30 @@
 
 import UIKit
 
-class CheckoutViewController: UIViewController {
+class CheckoutViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let data = [
+            ["10:00 AM", "2023-11-13", "Spot A", "cruiseImage1"],
+            ["02:30 PM", "2023-11-14", "Spot B", "cruiseImage2"],
+            // Add more data as needed
+        ]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("Number of rows: \(data.count)")
+            return data.count
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "itineraryCell", for: indexPath) as! ItineraryTableViewCell
+
+            // Configure the cell with data from your model
+            cell.timeLabel.text = data[indexPath.row][0]
+            cell.dateLabel.text = data[indexPath.row][1]
+            cell.spotNameLabel.text = data[indexPath.row][2]
+            cell.spotImageView.image = UIImage(named: data[indexPath.row][3])
+
+            return cell
+        }
 
     @IBOutlet weak var ticketTitle: UILabel!
     
@@ -55,4 +78,11 @@ class CheckoutViewController: UIViewController {
         self.view.sendSubviewToBack(cardView)
     }
      */
+}
+
+class ItineraryTableViewCell: UITableViewCell {
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var spotNameLabel: UILabel!
+    @IBOutlet weak var spotImageView: UIImageView!
 }
